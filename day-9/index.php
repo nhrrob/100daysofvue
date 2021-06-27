6aca53e7af6a8a6025156304acb396ccb694ecc8
Nazmul Hasan Robin <robin.sust08@gmail.com>
2021-06-27 11:15:17 +0600

    day 9 initial commit

---
+++ day-9/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>#100DaysOfVue</title>
+</head>
+
+<body>
+    <h1>Day 9 - Learn Vue Js </h1>
+
+    <div id="app">
+        <task-list></task-list>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/vue@2.6.14/dist/vue.js"></script>
+
+    <script src="main.js"></script>
+
+</body>
+
+</html>
